9f587a3265743da147cee0f0de9cdd8382460d7f
yurihallan <yurihallan9@gmail.com>
2021-02-06 11:31:44 -0400

    Segunda parte da aula Quiz alura. Input e botao na tela.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import QuizBackground from '../src/components/QuizBackground';
 import GitHubCorner from '../src/components/GithubCorner';
-
-
+import Input from '../src/components/Input';
 
 // const BackgroundImage = styled.div`
 //   background-image:url(${db.bg});
@@ -13,7 +15,6 @@ import GitHubCorner from '../src/components/GithubCorner';
 //   background-size:cover;
 //   background-position:center;
 // `;
-
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -26,30 +27,53 @@ export const QuizContainer = styled.div`
   }
 `;
 
-
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+
       <QuizContainer>
-        
         <Widget>
-            <Widget.Header>
-              <h1>Quizz de Anime</h1>
-            </Widget.Header>
+          <Widget.Header>
+            <h1>Quizz de Anime</h1>
+          </Widget.Header>
           <Widget.Content>
-              <p>lorem ipsum dolor sit</p>
+            <form onSubmit={function (event) {
+              event.preventDefault();
+
+              router.push(`/Quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  console.log(infosDoEvento.target.value);
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Diz ai seu nome"
+              />
+
+              <Input />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {' '}
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
         <Widget>
           <Widget.Content>
-          <h1>Quizes da galera</h1>
-          <p>loram ipsum dolor sit</p>
+            <h1>Quizes da galera</h1>
+            <p>loram ipsum dolor sit</p>
           </Widget.Content>
         </Widget>
-        <Footer/>
+        <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/yurihallan/aluraquiz-base"/>
+      <GitHubCorner projectUrl="https://github.com/yurihallan/aluraquiz-base" />
     </QuizBackground>
-  )
+  );
 }
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,14 +1,10 @@
-import styled from 'styled-components'
-
+import styled from 'styled-components';
 
 const Widget = styled.div`
 margin-top:24px;
 margin-bottom:24px;
-border:1px solid ${({theme}) => theme.colors.primary};
-background-color:${({theme}) => {
-    
-    return theme.colors.mainBg;
-}};
+border:1px solid ${({ theme }) => theme.colors.primary};
+background-color:${({ theme }) => theme.colors.mainBg};
 border-radius:4px;
 overflow:hidden;
 
@@ -31,9 +27,8 @@ display:flex;
 justify-content:flex-start;
 align-items:center;
 padding:18px 32px;
-background-color: ${({theme}) => theme.colors.primary};
+background-color: ${({ theme }) => theme.colors.primary};
 `;
-
 
 Widget.Content = styled.div`
 padding:24px 32px 32px 32px;
