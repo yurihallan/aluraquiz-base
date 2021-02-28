5515defb0d1c20f07e3565dbf58b0575af9e469b
yurihallan <yurihallan9@gmail.com>
2021-02-28 13:46:13 -0400

    Modificacoes no db.json, quiz.js e index.js - novas paginas e arquivos adicionados.

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
-/* eslint-disable import/no-unresolved */
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { motion } from 'framer-motion';
 
 import db from '../db.json';
 import Widget from '../src/components/Widget';
+import Link from '../src/components/Link';
 import Footer from '../src/components/Footer';
 import QuizBackground from '../src/components/QuizBackground';
 import QuizLogo from '../src/components/QuizLogo';
-import QuizContainer from '../src/components/QuizContainer';
+// import QuizContainer from '../src/components/QuizContainer';
 import GitHubCorner from '../src/components/GithubCorner';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
@@ -20,16 +22,16 @@ import Button from '../src/components/Button';
 //   background-position:center;
 // `;
 
-// export const QuizContainer = styled.div`
-//   width: 100%;
-//   max-width: 350px;
-//   padding-top: 45px;
-//   margin: auto 10%;
-//   @media screen and (max-width: 500px) {
-//     margin: auto;
-//     padding: 15px;
-//   }
-// `;
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
   const router = useRouter();
@@ -37,18 +39,34 @@ export default function Home() {
 
   return (
     <QuizBackground backgroundImage={db.bg}>
-
+      <Head>
+        <title>
+          AluraQuiz -
+          {db.title}
+        </title>
+      </Head>
       <QuizContainer>
         <QuizLogo />
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
-            <h1>Quizz de Anime</h1>
+            <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
+            <p>{db.description}</p>
             <form onSubmit={function (event) {
               event.preventDefault();
-
-              router.push(`/Quiz?name=${name}`);
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
             }}
             >
               <Input
@@ -65,13 +83,51 @@ export default function Home() {
           </Widget.Content>
         </Widget>
 
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.7, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Content>
             <h1>Quizes da galera</h1>
-            <p>loram ipsum dolor sit</p>
+            <ul>
+              {db.external.map((linkExterno) => {
+                const [projectName, githubUser] = linkExterno
+                  .replace(/\//g, '')
+                  .replace('https:', '')
+                  .replace('.vercel.app', '')
+                  .split('.');
+
+                return (
+                  <li key={linkExterno}>
+                    <Widget.Topic
+                      as={Link}
+                      href={`/quiz/${projectName}___${githubUser}`}
+                    >
+                      {`${githubUser}/${projectName}`}
+                    </Widget.Topic>
+                  </li>
+                );
+              })}
+            </ul>
           </Widget.Content>
         </Widget>
-        <Footer />
+        <Footer
+          as={motion.footer}
+          transition={{ delay: 1, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/yurihallan/aluraquiz-base" />
     </QuizBackground>
